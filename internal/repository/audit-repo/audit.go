607d975ad7146dd48de2567ec7c2f0fe0a0fca85
Rishi-Go <rishi.k@mitrahsoft.in>
2026-08-25 12:01:31 +0530

    fix(repository): fix ambiguous db queries and add async audit log creation (#146)

--- internal/repository/audit-repo/audit.go
+++ internal/repository/audit-repo/audit.go
@@ -14,14 +14,11 @@ import (
 )
 
 func (d *auditDatabase) CreateAuditLog(log models.AuditLog) *response.Error {
-	if err := d.db.Create(&log).Error; err != nil {
-		d.logger.Error("Database error occurred while creating audit log", zap.Error(err))
-		return &response.Error{
-			Code:       response.ErrInternalServerError,
-			StatusCode: http.StatusInternalServerError,
-			Message:    "Something went wrong. Please try again later.",
+	go func() {
+		if err := d.db.Create(&log).Error; err != nil {
+			d.logger.Error("Database error occurred while creating audit log asynchronously", zap.Error(err))
 		}
-	}
+	}()
 	return nil
 }
 
